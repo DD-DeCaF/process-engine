b1fdedbed6b58878a53e8bab914b8bfce8e643c2
Svetlana <galkina.s.a@gmail.com>
2017-12-21 14:31:15 +0100

    fix: works without docker

---
+++ modeling/__init__.py
@@ -0,0 +1,6 @@
+"""Genialis Platform Django app."""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
--- modeling/celery.py
+++ modeling/celery.py
@@ -1,24 +1,22 @@
+"""Celery configuration."""
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-"""
-=============
-Celery Config
-=============
-Celery preferences and general tasks.
-"""
-from __future__ import absolute_import
-import logging
 import os
 
-from celery import Celery
+try:
+    from celery import Celery
+except ImportError:
+    Celery = None
 
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modeling.settings')
 
-app = Celery('modeling')  # pylint: disable=invalid-name
-logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "modeling.settings")
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app = None  # pylint: disable=invalid-name
+if Celery:
+    app = Celery('modeling')  # pylint: disable=invalid-name
+
+    app.config_from_object('django.conf:settings')
+    app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
